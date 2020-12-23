6329f3a0262b24e739af62a00197bb8586943b56
Kevin-liu07 <2558261731@qq.com>
2020-12-23 18:11:39 +0800

    12.23学习

---
+++ 12.23/温度转换.py
@@ -0,0 +1,11 @@
+
+#TemConvert.py
+tempstr=input("输入带有符号的温度值")
+if tempstr[-1] in ['F','f']:
+    C = (eval(tempstr[0:-1])-32)/1.8
+    print("转换后的温度是{:.2f}C".format(C))
+elif tempstr[-1] in ['C','c']:
+    F = 1.8*eval(tempstr[0:-1])+32
+    print("转换后的温度是{:.2f}F".format(F))
+else:
+    print("输入格式错误")
