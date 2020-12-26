3290e317571a5d62b9cde6d120fc7f201cb9f4d1
Kevin-liu07 <2558261731@qq.com>
2020-12-26 21:19:04 +0800

    12.26学习

---
+++ tset12.26/温度转换.py
@@ -0,0 +1,10 @@
+# tem.py
+tem = input("输入带有符号的温度")
+if tem[-1] in ['f','F']:
+    c = (eval(tem[0:-1])-32)/1.8
+    print("摄氏度为{:.2f}c".format(c))
+elif tem[-1] in ['c','C']:
+    f = 1.8*eval(tem[0:-1])+32
+    print("华氏度为{:.2f}f".format(f))
+else:
+    print("格式错误")
